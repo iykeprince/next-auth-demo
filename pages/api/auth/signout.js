54ff24e7d7819e4c408e9d03d94c1f3c372610e1
Iykeprince <iykeprince48@gmail.com>
2023-01-20 14:54:09 +0100

    server setup

---
+++ pages/api/auth/signout.js
@@ -0,0 +1,17 @@
+import { withIronSessionApiRoute  }  from "iron-session/next";
+
+const cookie= {
+    cookieName: process.env.COOKIE_NAME,
+    password: process.env.COOKIE_PASSWORD,
+    cookieOption: {
+        secure: process.env.NODE_ENV === 'production',
+    }
+}
+
+export default withIronSessionApiRoute(
+    function signOut(req, res) {
+        req.session.destroy();
+        res.status(200).json({ok: true})
+    },
+    cookie
+)
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,50 @@
+import { GraphQLClient, gql } from 'graphql-request'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+const {
+  GRAPHCMS_URL,
+  GRAPHCMS_PERMANENTAUTH_TOKEN,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+} = process.env
+
+const client = new GraphQLClient(GRAPHCMS_URL, {
+  headers: {
+    Authorization: `Bearer ${GRAPHCMS_PERMANENTAUTH_TOKEN}`,
+  },
+})
+
+const CreateNextUserMutation = gql`
+  mutation createNextUser($userData: NextUserCreateInput) {
+    createNextUser(data: $userData) {
+      id
+      email
+      token
+    }
+  }
+`;
+
+export default async function handler(req, res) {
+    const { email, password, firstname, lastname } = req.body;
+
+    if(!email || !password || !firstname || !lastname) {
+        res.status(400).end()
+    }
+
+    const token = jwt.sign({email}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+    const hashedPassword = await bcrypt.hash(password, 8)
+    const userData = {
+        email,
+        passowrd: hashedPassword,
+        firstname,
+        lastname,
+        token
+    }
+
+    const response = await client.request(CreateNextUserMutation, {userData});
+    if(!response?.createNextUser?.id) {
+        res.status(500)
+    }
+    res.status(200).json({token})
+}
